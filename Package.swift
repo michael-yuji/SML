d130ac1e3980a49e9038f1fb0a808c6f3c0dc4f3
michael-yuji <michael-yuji@users.noreply.github.com>
2016-10-25 23:28:11 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,5 @@ import PackageDescription
 
 let package = Package(
     name: "SMLKit",
-    dependencies: [.Package(url: "https://github.com/michael-yuji/SXF97.git", versions: Version(0,0,0)..<Version(1,0,1))]
+    dependencies: [.Package(url: "https://github.com/projectSX0/SXF97.git", versions: Version(0,0,0)..<Version(1,0,1))]
 )
